7d1d50f6cd443faae5ed104f120e486d1117ef77
niksnimje <niksnimje4@gmail.com>
2024-09-20 17:53:45 +0530

    Home Page Update

--- src/Page/Homepage.jsx
+++ src/Page/Homepage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
+import video from "../Video/Main-1.mp4"
+import video2 from "../Video/VIDEO1.mp4"
 // import './App.css'
 
 
@@ -61,22 +63,34 @@ function Homepage() {
       <div className="container-fluid p-0">
         <div className=" yellow-bg-img " >
          <div className='d-flex flex-column justify-content-center align-items-center h-100' >
-          <img src="https://www.aeropostale.com/on/demandware.static/-/Sites-aeropostale-Library/default/dw956e2e28/images/2024/Home/071024/A_SPOT_BTS%20DEALS_DT.png"   className='yellow-main-img'    alt="" /> <br />
-          <p> 
-          Stock up for less than your favorite latte. 
-          </p>
-          
+          <video src={video2} width="100%" playsInline muted autoPlay loop>
+            
+          </video>
+
           </div>
          </div>
       </div>
 
     <br />
 
+    <div className="container-fluid p-0">
+        <div className=" yellow-bg-img " >
+         <div className='d-flex flex-column justify-content-center align-items-center h-100' >
+          <video src={video}  width="100%" muted playsInline  autoPlay loop>
+            {/* <source  type="video/mp4"  /> */}
+          </video>
+
+          </div>
+         </div>
+      </div>
+
+<br /><br /><br />
 
     {/* first-img */}
 
         <Link to={"/jeans"}  style={{textDecoration:"none"}}  >
         <div className="container-fluid Jeans-bg p-0 d-flex justify-content-evenly  align-items-center flex-column flex-sm-column flex-md-column flex-lg-row">
+          
           
           
           <div className="col-12 col-sm-12 col-md-6 col-lg-3 p-0 im-1 mt-5">
@@ -131,51 +145,15 @@ function Homepage() {
       <br /><br />
 
     
-      {/* blue Image */}
-      <Link to={"/women"} style={{textDecoration:"none"}}>
-      <div className="container-fluid blue-img ">
-        <div className="row g-3 g-sm-0 text-center d-flex align-items-center pt-4 align-content-center flex-column flex-sm-row flex-md-row flex-lg-row">
-          <div className="col-3"></div>
-          <div className="col-12 col-sm-3 col-md-3">
-            <h1  style={{color:"#f7d309",fontWeight:"700"}}>
-            A SHORT STORY</h1>
-          </div>
-          <div className="col-12 col-sm-3 col-md-3 ">
-            <img src="https://www.aeropostale.com/on/demandware.static/-/Sites-aeropostale-Library/default/dw25fa6242/images/2024/Home/072424/Shorts%20Badge-min.png" width={200} alt="" />
-          </div>
-          <div className="col-12 col-md-3">
-          <button className='btn-women-men ' ><Link to={"/women"} className=' women-btn '>Women</Link> &nbsp;&nbsp;&nbsp;&nbsp; <Link to={"/men"} className='men-btn'>Men</Link> </button>
-          </div>
-        </div>
-      </div>
-      </Link>
-
+     
     <br />
 
       {/* Uniform Image */}
 
-      <Link to={"/women"}>
-      <div className="container-fluid uniform-img ">
-
-      </div>
-      </Link>
+  
 
       <br />
-      {/* smile Image 2*/}
-      <Link to={"/women"}>
-      <div className="container-fluid smile-img ">
-        <div className="row g-3 g-sm-0 text-center d-flex align-items-center align-content-center flex-column flex-sm-row flex-md-row flex-lg-row">
-          <div className="col-3"></div>
-          <div className="col-12 col-sm-5 col-lg-5 im-6 ">
-            
-          </div>
-          
-          <div className="col-12  col-md-3">
-          <button className='btn-women-men ' ><Link to={"/women"} className=' women-btn '>Women</Link> &nbsp;&nbsp;&nbsp;&nbsp; <Link to={"/men"} className='men-btn'>Men</Link> </button>
-          </div>
-        </div>
-      </div>
-      </Link>
+    
 
 
 
